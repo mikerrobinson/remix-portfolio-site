45dde5b7672f082c0010b74a116ea9a50555fc67
Mike Robinson <mikerrobinson@hotmail.com>
2025-09-24 12:58:40 -0700

    revert removal of minification in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  build: {
-    minify: false,
-  },
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
